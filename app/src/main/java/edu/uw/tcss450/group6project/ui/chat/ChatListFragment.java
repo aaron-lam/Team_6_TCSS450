ed27418a421f11673c7f38ca8dc1c8c27e0bd611
Robert Mangrum <rmangrum@uw.edu>
2020-10-27 20:09:23 -0700

    Added placeholder pages

---
+++ app/src/main/java/edu/uw/tcss450/group6project/ui/chat/ChatListFragment.java
@@ -0,0 +1,24 @@
+package edu.uw.tcss450.group6project.ui.chat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.group6project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+}
