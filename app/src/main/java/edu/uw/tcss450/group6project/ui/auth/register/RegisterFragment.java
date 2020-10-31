d33b44d30d938d431071c2460a189958adddf875
aldrchase <aldrchase1@gmail.com>
2020-10-31 13:03:12 -0700

    Users can now register with the web service.

--- app/src/main/java/edu/uw/tcss450/group6project/ui/auth/register/RegisterFragment.java
+++ app/src/main/java/edu/uw/tcss450/group6project/ui/auth/register/RegisterFragment.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.uw.tcss450.group6project.R;
 import edu.uw.tcss450.group6project.databinding.FragmentRegisterBinding;
@@ -21,6 +26,14 @@ import edu.uw.tcss450.group6project.utils.RegisterValidator;
 public class RegisterFragment extends Fragment {
 
     private FragmentRegisterBinding binding;
+    private RegisterViewModel mRegisterModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRegisterModel = new ViewModelProvider(getActivity())
+                .get(RegisterViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,8 +50,43 @@ public class RegisterFragment extends Fragment {
             RegisterValidator registerValidator = new RegisterValidator(binding);
 
             if (registerValidator.validateAll()) {
-                Navigation.findNavController(getView()).navigate(RegisterFragmentDirections.actionRegisterFragmentToSignInFragment());
+                verifyAuthWithServer();
             }
+
+            mRegisterModel.addResponseObserver(getViewLifecycleOwner(),
+                    this::observeResponse);
         });
+    }
+
+    private void successfulSignIn() {
+        Navigation.findNavController(getView()).navigate(RegisterFragmentDirections.actionRegisterFragmentToSignInFragment());
+    }
+
+    private void verifyAuthWithServer() {
+        mRegisterModel.connect(
+                binding.fieldRegisterFirstName.getText().toString(),
+                binding.fieldRegisterLastName.getText().toString(),
+                binding.fieldRegisterEmail.getText().toString(),
+                binding.fieldRegisterPassword.getText().toString());
+        //This is an Asynchronous call. No statements after should rely on the
+        //result of connect().
+    }
+
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.fieldRegisterEmail.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                successfulSignIn();
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
     }
 }
