8f7b9f9ec3324846542edb2c66176af6a9788710
Anthony <nguyenanthony80@gmail.com>
2020-11-17 17:02:39 -0800

    Removed unecessary variables.

--- app/src/main/java/edu/uw/tcss450/group6project/ui/chat/ChatFragment.java
+++ app/src/main/java/edu/uw/tcss450/group6project/ui/chat/ChatFragment.java
@@ -50,9 +50,8 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_chat, container, false);
-
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
     }
 
     @Override
